0ca938698424b4afaef18e468204dafd32e27c6e
antoan-97 <antoan.todorov@abv.bg>
2024-05-22 14:52:32 +0300

    create managers

---
+++ src/managers/photoManager.js
@@ -0,0 +1,8 @@
+const Photo = require('../models/Photo');
+
+
+exports.create = (photoData) => Photo.create(photoData);
+
+exports.getAll = () => Photo.find().populate('owner');
+
+exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
