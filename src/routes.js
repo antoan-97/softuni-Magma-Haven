f47e6023defd95810f27d83dfb14631f333d1f2c
antoan-97 <antoan.todorov@abv.bg>
2024-05-22 14:52:05 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const photoController = require('./controllers/photoController');
 
 
 
 router.use(homeController);
-router.use('/users',userController);
+router.use('/users', userController);
+router.use('/photos', photoController);
 
 
 module.exports = router
