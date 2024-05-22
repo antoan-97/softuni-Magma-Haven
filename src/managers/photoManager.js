849423f9fa65b341d40f3089702b253e33555267
antoan-97 <antoan.todorov@abv.bg>
2024-05-22 16:22:10 +0300

    create search manager

--- src/managers/photoManager.js
+++ src/managers/photoManager.js
@@ -10,3 +10,7 @@ exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
 exports.delete = (photoId) => Photo.findByIdAndDelete(photoId);
 
 exports.edit = (photoId,photoData) => Photo.findByIdAndUpdate(photoId, photoData);
+
+exports.search = (query) => {
+    return Photo.find(query).lean();
+};
