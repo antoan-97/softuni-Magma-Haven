e04e6ceda070fa6a7174d705415d59a60c9bfa12
antoan-97 <antoan.todorov@abv.bg>
2024-05-23 15:17:44 +0300

    Add validations and error messages for Photo schema

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -4,22 +4,28 @@ const photoSchema = new mongoose.Schema({
     name:{
         type:String,
         required: true,
+        minLength: [2, 'Name should be at least 2 characters!'],
     },
     location:{
         type:String,
         required:true,
+        minLength: [3, 'Location should be at least 3 characters!'],
     },
     elevation:{
         type:Number,
         required:true,
+        min: [0, 'Elevation should be minimum 0!']
     },
     lastEruption:{
         type:Number,
         required:true,
+        min: [0, 'The Year of last eruption should be between 0 and 2024!'],
+        max: [2024, 'The Year of last eruption should be between 0 and 2024!']
     },
     image:{
         type:String,
         required:true,
+        match:[/^https?:\/\//, 'Invalid URL!']
     },
     typeVolcano:{
         type:String,
@@ -29,6 +35,7 @@ const photoSchema = new mongoose.Schema({
     description:{
         type:String,
         required:true,
+        minLength: [10,'Description should be at least 10 characters!']
     },
     voteList:[{
         type: mongoose.Schema.Types.ObjectId,
