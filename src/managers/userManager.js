3197c9ee42cab7217640e991aa8d20e05e3d53aa
antoan-97 <antoan.todorov@abv.bg>
2024-06-12 14:30:18 +0300

    use login middleware validator in routers

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
 
+
 const { SECRET } = require('../config/config');
 
 exports.register = async (userData) => {
